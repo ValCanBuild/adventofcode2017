f862e490d9d54b62b06d72b8f6529c1f2c4df452
Valentin Hinov <val@rockspin.co.uk>
2017-12-04 09:31:22 +0000

    Solutions for day4

---
+++ AdventDay4.playground/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import Foundation
+import PlaygroundSupport
+
+let inputUrl = Bundle.main.url(forResource: "AdventDay4Input", withExtension: ".txt")!
+let input = try String(contentsOf: inputUrl)
+let lines = input.components(separatedBy: .newlines)
+
+func findValidPhrases(_ includeAnagrams: Bool) -> Int {
+    return lines.map {
+        let spaceSeparatedWords = $0.components(separatedBy: .whitespaces)
+        let words = includeAnagrams ? spaceSeparatedWords.map { String($0.sorted()) } : spaceSeparatedWords
+        let set = Set(words)
+        return set.count == words.count
+        }
+        .filter { $0 == true }
+        .count
+    
+}
+
+print("Num valid for part1 is \(findValidPhrases(false))")
+print("Num valid for part2 is \(findValidPhrases(true))")
